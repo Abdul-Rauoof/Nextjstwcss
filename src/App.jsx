f204f36f869ab58e99e6596ebbffc27f56f56125
Abdul-Rauoof <abdul.rauoof412ar@gmail.com>
2024-04-23 15:34:29 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { useInView } from "react-intersection-observer"
+
+function App() {
+
+  return (
+    <>
+      <p className="text-cyan-500">nextjs and tailwind css</p>
+
+    </>
+  )
+}
+
+export default App
